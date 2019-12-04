6bb843c39317011e52718598c7c7f8d80edab29d
Velibor Zeli <velibor@mech.kth.se>
2019-12-05 00:01:53 +0100

    Change optional argument `--list` to subcommand

--- main.py
+++ main.py
@@ -36,8 +36,12 @@ def remove(args):
     io_utils.write(reg)
 
 
-parser = argparse.ArgumentParser(prog="iwime")
-parser.add_argument("-ls", "--list", action="store_true", help="list all records")
+def list_records(args):
+    reg = io_utils.read()
+    reg.as_table()
+
+
+parser = argparse.ArgumentParser(prog="iwry")
 subparsers = parser.add_subparsers(title="subcommands", help="sub-command help")
 
 parser_add = subparsers.add_parser("add", help="add records")
@@ -48,13 +52,13 @@ parser_remove = subparsers.add_parser("remove", help="remove records")
 parser_remove.add_argument("n", type=int, help="number of records to remove", default=1)
 parser_remove.set_defaults(func=remove)
 
+parser_show = subparsers.add_parser("show", help="show records")
+parser_show.set_defaults(func=list_records)
+parser_show.add_argument("--table", help="list all records")
+
 parser_init = subparsers.add_parser("init", help="initialize records")
 parser_init.set_defaults(func=init)
 
 args = parser.parse_args()
 
-if args.list:
-    reg = io_utils.read()
-    reg.as_table()
-else:
-    args.func(args)
+args.func(args)
